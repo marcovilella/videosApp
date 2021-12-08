a8bcd9f77b98f0f3f925ac9059d3b8cc3524cba1
Marco Antonio Goncalves Vilella <marcoagvillela@hotmail.com>
2021-12-08 16:42:00 -0300

    Momento em que parei no dia 08/12/2021.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.models';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,29 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos';
+
+  listarVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/44aCR6cjH0FbzW6PMw3Ega178iW.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+      nome: '',
+      lancamento: '',
+      duracao: '',
+      classificacao: 0,
+      cartaz: '',
+      generos: []
+
+    }
+
+  ];
 
   constructor(
     public alertController: AlertController,
